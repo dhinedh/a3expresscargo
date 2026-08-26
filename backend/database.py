15cc3a971fbfe1fb391f70a86267410088a7b0f0
zechsoft <zechsoft.it@gmail.com>
2026-08-26 10:05:41 +0530

    Configure MongoDB Atlas credentials and update requirements.txt

--- backend/database.py
+++ backend/database.py
@@ -4,6 +4,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 import os
 
+try:
+    from pymongo import MongoClient
+except ImportError:
+    MongoClient = None
+
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./tariff.db")
 
 engine = create_engine(
@@ -13,6 +18,22 @@ engine = create_engine(
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+# MongoDB Atlas Integration
+MONGODB_URL = os.getenv(
+    "MONGODB_URL",
+    "mongodb+srv://thenna44ck_db_user:2dWQ2jrV762IvKs6@cluster0.phdzsgq.mongodb.net/a3_express?retryWrites=true&w=majority&appName=Cluster0"
+)
+
+_mongo_client = None
+
+def get_mongo_db():
+    global _mongo_client
+    if MongoClient is None:
+        return None
+    if _mongo_client is None:
+        _mongo_client = MongoClient(MONGODB_URL)
+    return _mongo_client["a3_express"]
 
 def get_db():
     db = SessionLocal()
